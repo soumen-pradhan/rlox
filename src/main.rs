cb6c782775393ab660859edb09e8c9402076c5f4
soumen-pradhan <sam.thepradhans@gmail.com>
2023-07-31 18:48:05 +0530

    bug: Added newlines at the end

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use error::ErrorLogger;
+use lexer::Lexer;
 
 mod chunk;
+mod error;
+mod lexer;
 mod utils;
 mod value;
 mod vm;
-mod lexer;
 
 fn main() {
-    // let file = File::open("test/sample.lox").unwrap();
-    // let buffer = BufReader::new(file);
+    let file = File::open("test/sample.lox").unwrap();
+    let buffer = BufReader::new(file);
 
-    // let lines = buffer
-    //     .lines()
-    //     .filter_map(|line| line.ok())
-    //     .enumerate()
-    //     .collect::<Vec<_>>();
+    let lines = buffer
+        .lines()
+        .filter_map(|line| line.ok().map(|l| l + "\n"))
+        .collect::<Vec<_>>();
 
-    // for ((line_no, col), line) in lines
-    //     .iter()
-    //     .flat_map(|(line_no, line)| line.char_indices().map(|(col, c)| ((*line_no, col), c)))
-    // {
-    //     println!("[{:5}:{:3}] {line}", line_no + 1, col + 1);
-    // }
+    let logger = ErrorLogger { lines: &lines };
+
+    for token in Lexer::new(&lines, &logger).symbols() {
+        println!("{token:?}");
+    }
 }
