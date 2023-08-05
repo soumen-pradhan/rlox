35420b893740b73da90a3959fa9b9919c2b27341
soumen-pradhan <sam.thepradhans@gmail.com>
2023-08-05 18:35:02 +0530

    feat: Added Value convert to bool

--- src/value.rs
+++ src/value.rs
@@ -2,21 +2,36 @@
 
 use std::fmt::Display;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub enum Value {
+    Bool(bool),
+    Nil,
     Num(f64),
 }
 
-const EPSILON: f64 = 1e-200;
+impl Value {
+    pub fn is_falsey(&self) -> bool {
+        use Value::*;
+
+        match self {
+            Bool(false) | Nil => false,
+            _ => true,
+        }
+    }
+}
 
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Value::*;
+
         match self {
-            Value::Num(n) => write!(
-                f,
-                "{n:.precision$}",
-                precision = if n.fract() > EPSILON { 2 } else { 0 }
-            ),
+            Bool(b) => write!(f, "{b}"),
+            Nil => write!(f, "nil"),
+
+            Num(n) => {
+                let precision = if n.abs().fract() > f64::EPSILON { 2 } else { 0 };
+                write!(f, "{n:.precision$}")
+            }
         }
     }
 }
@@ -47,20 +62,37 @@ impl ValuePool {
     }
 }
 
+fn find_precision(n: f64, max_prec: usize) -> usize {
+    const EPSILON: f64 = 1e-8;
+
+    let mut n = n.abs();
+    let mut precision = 0;
+
+    while n.fract() > f64::EPSILON && precision < max_prec {
+        println!("    {}", n.fract());
+        n *= 10.0;
+        precision += 1;
+    }
+    println!("end    {}", n.fract());
+
+    precision
+}
+
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
     fn check_fractional_printing() {
-        let n: f64 = 3.14;
-        let n2: f64 = 3.0;
+        const EPSILON: f64 = 1e-8;
+        println!("{EPSILON}");
 
-        println!("{n:.width$}", width = if n.fract() > 1e-6 { 2 } else { 0 });
-        println!(
-            "{n2:.width$}",
-            width = if n2.fract() > 1e-6 { 2 } else { 0 }
-        );
-        println!("{}", f64::MIN_POSITIVE);
+        for n in [1.0, -1.0, 1.10, 1.12, 1.123, 1.1230001, 1.12345] {
+            println!(
+                "{n} | {n:.p$} | {}",
+                (n as f64).fract(),
+                p = find_precision(n, 3)
+            );
+        }
     }
 }
